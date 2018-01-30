176e15e180f0ebb6b213b1bffda19668deaf2032
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2018-01-30 15:05:31 -0500

    re-org directories, top-level analysis directory

---
+++ analysis/bw/bw_atmos_eddies/fre-analysis/bw_atmos_eddies/shared/bw/ncl/wrapit/horiz_interp_float.f90
@@ -0,0 +1,23 @@
+
+subroutine horiz_interp_float ( nxi, nyi, nk, nl, blon_in, blat_in, data_in, &
+                          nxo, nyo, blon_out, blat_out, data_out, &
+                          critical_frac, missing_value, verbose, error_status )
+implicit none
+
+integer, intent(in)  :: nxi, nyi, nk, nl, nxo, nyo
+real*8, intent(in)   :: blon_in(2,nxi) , blat_in(2,nyi)
+real*8, intent(in)   :: blon_out(2,nxo), blat_out(2,nyo)
+real, intent(in)     :: data_in(nxi,nyi,nk,nl)
+real, intent(out)    :: data_out(nxo,nyo,nk,nl)
+real, intent(in)     :: critical_frac, missing_value
+integer, intent(in)  :: verbose
+integer, intent(out) :: error_status
+
+!-----------------------------------------------------------------------
+ 
+include 'horiz_interp.inc'
+
+!-----------------------------------------------------------------------
+
+ end subroutine horiz_interp_float
+
