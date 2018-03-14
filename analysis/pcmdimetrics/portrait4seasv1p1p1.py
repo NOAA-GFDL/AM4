965d64e9c03219a63eeac8f115b287730b9231ef
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2018-03-14 15:43:10 -0400

    extraneous characters rm

--- analysis/pcmdimetrics/portrait4seasv1p1p1.py
+++ analysis/pcmdimetrics/portrait4seasv1p1p1.py
@@ -219,10 +219,7 @@ if __name__ == "__main__":
                         default="pr,psl,tas,rlut,rsut,ua-850,ua-200,va-850,va-200,zg-500", required=False)
     parser.add_argument("-x", "--exclude", help="Comma delimited list to exclude CMIP5 models.", required=False)
     parser.add_argument("-m", "--mode", help="The option to change which version of the data to compare against.", default="amip")
-<<<<<<< HEAD
-=======
     parser.add_argument("-s", "--cmipMetrics", help="specifies metrics location  computed from other CMIP models. specify path till share directory only", default="/home/a1r/metrics_latest_v1p1p1/share/")
->>>>>>> 76717f2ec0e6c273e9b68e60eaf5a8f0057abe64
     parser.add_argument("argv", nargs=argparse.REMAINDER)
     args = parser.parse_args()
  
