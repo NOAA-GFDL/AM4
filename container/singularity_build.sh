3c0b90e7c70fc7f6daefcd1963650fbd7e0fd055
Tom Robinson <thomas.robinson@noaa.gov>
2021-05-26 06:52:41 -0400

    Updates to specify ubuntu build

--- container/singularity_build.sh
+++ container/singularity_build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 singularity build -f intel_netcdf_ubuntu.sif Singularity.intel_netcdf
-singularity build -f am4_ubuntu_intel.sif Singularity.intel_am4
+singularity build -f am4_2021.02_ubuntu_intel.sif Singularity.intel_am4
